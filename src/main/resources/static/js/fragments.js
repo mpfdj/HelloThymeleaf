26c8df6975d25a28fe5f7742187d71fbfe777bad
miel <miel.de.jaeger@ing.nl>
2023-02-08 13:16:09 +0100

    test-with-sort finished

---
+++ src/main/resources/static/js/fragments.js
@@ -0,0 +1,24 @@
+const fragmentSortByNameAsc          = '<th style="" data-field="name" id="btnSortByNameAsc"><div class="th-inner ">name <i class="bi bi-sort-up"></i></div><div class="fht-cell"></div></th>';
+const fragmentSortByNameDesc         = '<th style="" data-field="name" id="btnSortByNameDesc"><div class="th-inner ">name <i class="bi bi-sort-down"></i></div><div class="fht-cell"></div></th>';
+const fragmentSortBySizeAsc          = '<th style="" data-field="size" id="btnSortBySizeAsc"><div class="th-inner ">size <i class="bi bi-sort-up"></i></div><div class="fht-cell"></div></th>';
+const fragmentSortBySizeDesc         = '<th style="" data-field="size" id="btnSortBySizeDesc"><div class="th-inner ">size <i class="bi bi-sort-down"></i></div><div class="fht-cell"></div></th>';
+const fragmentSortByLastModifiedAsc  = '<th style="" data-field="size" id="btnSortByLastModifiedAsc"><div class="th-inner ">last modified <i class="bi bi-sort-up"></i></div><div class="fht-cell"></div></th>';
+const fragmentSortByLastModifiedDesc = '<th style="" data-field="size" id="btnSortByLastModifiedDesc"><div class="th-inner ">last modified <i class="bi bi-sort-down"></i></div><div class="fht-cell"></div></th>';
+
+const fragmentDefaultTableHeader = '\
+<thead> \
+<tr> \
+   <th style="" data-field="name" id="btnSortByNameAsc"> \
+       <div class="th-inner ">name</div> \
+       <div class="fht-cell"></div> \
+   </th> \
+   <th style="" data-field="size" id="btnSortBySizeAsc"> \
+       <div class="th-inner ">size</div> \
+       <div class="fht-cell"></div> \
+   </th> \
+   <th style="" data-field="last_modified" id="btnSortByLastModifiedAsc"> \
+       <div class="th-inner ">last modified</div> \
+       <div class="fht-cell"></div> \
+   </th> \
+</tr> \
+</thead>'
